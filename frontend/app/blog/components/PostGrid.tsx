f8593d40bba6a56e2139516424675b9fc92a68bf
Martin Gama Jr <marzsh@MacBookPro.lan>
2026-05-25 15:30:59 -0700

    fixed moblie view for blog

--- frontend/app/blog/components/PostGrid.tsx
+++ frontend/app/blog/components/PostGrid.tsx
@@ -15,7 +15,7 @@ export default function PostGrid({ posts }: PostGridProps) {
   }
 
   return (
-    <div className="grid grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
       {posts.map((post, index) => (
         <PostCard 
           key={post._id} 
